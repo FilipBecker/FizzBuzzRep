ff99a6820bb4a5acc682e205489438cf1cbb4e35
FilipBecker <filip.becker@edu.huddinge.se>
2023-09-12 14:54:59 +0200

    Numer som är multipel av 7 skriver ut Boom. PS jag vet att den överskrider de andra villkoren, uppgiften specifierade inte hur det skulle hanteras så jag valde det själv.

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,5 +1,7 @@
 for (var i = 1; i <= 200; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 7 === 0) {
+        console.log("Boom");
+    } else if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
     } else if (i % 5 === 0) {
         console.log("Buzz");
