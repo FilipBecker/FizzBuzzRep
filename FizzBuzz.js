37972b63270b17268c80f423f0bce951df91eb21
FilipBecker <filip.becker@edu.huddinge.se>
2023-09-12 15:02:41 +0200

    La till två parametrar start och end som bestämmer vart den ska böja och sluta.

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,13 +1,20 @@
-for (var i = 1; i <= 200; i++) {
-    if (i % 7 === 0) {
-        console.log("Boom");
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else {
-        console.log(i);
+const start = 20
+const end = 50
+
+function FizzBuzz(st, ed) {
+    for (var i = st; i <= ed; i++) {
+        if (i % 7 === 0) {
+            console.log("Boom");
+        } else if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else {
+            console.log(i);
+        }
     }
 }
+
+FizzBuzz(start, end)
